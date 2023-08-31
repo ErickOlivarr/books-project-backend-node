0bd06bff9c7c35fca99d61294057f4f9429b31b5
Erick Olivar <erick.salvador.olivar@gmail.com>
2023-08-30 19:45:26 -0600

    prueba y readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Backend en node.js para el proyecto de libros
+
+```
+npm install para instalar las dependencias
+```
--- app.ts
+++ app.ts
@@ -4,3 +4,5 @@ dotenv.config();
 
 const server = new Server();
 server.listen();
+
+//aqui se hizo un cambio para probar git y github
